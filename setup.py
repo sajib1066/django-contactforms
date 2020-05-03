4afbaf4121a49b89936963aa8c63572312b0d855
sajib1066 <sajib1066@gmail.com>
2020-05-03 19:24:53 +0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='django-contacts-us',  
+     version='0.1',
+     scripts=['django-contacts-us'] ,
+     author="Sajib Hossain",
+     author_email="sajib1066@gmail.com",
+     description="Django Contact Us Form Package",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/sajib1066/django-contacts-us",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
