42ab15d0fc70cd9564ef6ff0520224a07597fd8e
sajib1066 <sajib1066@gmail.com>
2020-05-08 12:21:48 +0600

    release v1.3

--- contact_forms/views.py
+++ contact_forms/views.py
@@ -1,18 +1,20 @@
-from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib import messages
 
-from .forms import ContactForm
-
+from .models import Contact
 
 def contact_page(request):
-    forms = ContactForm()
     if request.method == 'POST':
-        forms = ContactForm(request.POST)
-        if forms.is_valid():
-            forms.save()
-            messages.add_message(request, messages.INFO, 'Submitted!')
-            return redirect('home')
-    context = {
-        'forms': forms
-    }
-    return render(request, 'contact/contact.html', context)
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        try:
+            contacts = Contact.objects.create(name=name, email=email, subject=subject, message=message)
+            contacts.save()
+        except Exception as e:
+            return HttpResponse(e)
+        success_message = ('Successfully Send Feedback!')
+        return HttpResponse(success_message)
+    else:
+        return HttpResponse('Something Wrong')
